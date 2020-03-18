0976df91ed7985c04303a02fbcb0c8b4c7592338
Matteo Andruccioli <matteo.andruccioli2@studio.unibo.it>
2020-03-18 17:38:29 +0100

    [Matteo] Creato primo client e relativo launcher

---
+++ src/main/scala/client/ClientLauncher.scala
@@ -0,0 +1,15 @@
+package client
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+//launcher del client
+object ClientLauncher extends App{
+  val config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + 2552)
+    .withFallback(ConfigFactory.parseString("akka.cluster.roles = [clientRole]"))
+    .withFallback(ConfigFactory.load())
+
+  val system = ActorSystem.create("ClusterSystem", config)
+
+  system.actorOf(Props.create(classOf[ClientActor]), "client")
+}
