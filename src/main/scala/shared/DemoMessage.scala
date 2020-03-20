50c64d14e82cd6c6990a54dfd12ef11dc3e39d7d
Matteo Andruccioli <matteo.andruccioli2@studio.unibo.it>
2020-03-20 08:31:14 +0100

    [Matteo] Implementazione base di comunicazione sicura client Server

--- src/main/scala/shared/DemoMessage.scala
+++ src/main/scala/shared/DemoMessage.scala
@@ -4,8 +4,12 @@ package shared
 //sono solo dei messaggi di prova per verificare che la comunicazione funzioni
 sealed trait DemoMessage
 object DemoMessage {
-  //messaggio inviato dal Client
-  case class ClientMessage (message:String) extends DemoMessage
+  //messaggio inviato dal Client sul mediator
+  case class ClientMediatorMessage (message:String) extends DemoMessage
   //messaggio inviato dal Server
-  case class ServerMessage (message:String) extends DemoMessage
+  case class ServerMediatorMessage (message:String) extends DemoMessage
+  //ack inviato dal client alla ricezione di un messaggio ServerMediatorMessage
+  case class ClientAck () extends DemoMessage
+  //ack inviato dal server alla ricezione di un messaggio ClientMediatorMessage
+  case class ServerAck () extends DemoMessage
 }
