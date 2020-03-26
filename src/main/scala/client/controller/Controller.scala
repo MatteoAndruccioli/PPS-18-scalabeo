de84552c8091f96ab3a857b4f7e5814a272c2a7f
Michele Borasca <michele.borasca@gmail.com>
2020-03-26 18:42:52 +0100

    [mvc] Aggiunto un controller e la classe View che si occupa di gestire le varie schermate

---
+++ src/main/scala/client/controller/Controller.scala
@@ -0,0 +1,56 @@
+package client.controller
+
+import akka.actor.ActorRef
+import client.view.View
+
+object Controller {
+
+  private var _myTurn: Boolean = false
+  private var clientRef: ActorRef = _
+  private var _username: String = _
+
+  def username_= (username: String): Unit = _username = username
+  def username: String = _username
+
+  def init(clientRef: ActorRef): Unit  = {
+    this.clientRef = clientRef
+    startGui()
+  }
+
+  private def startGui(): Unit = {
+    new Thread(() => {
+      View.main(Array[String]())
+    }).start()
+  }
+
+  def onLoginResponse(): Unit = {
+    View.onLoginResponse()
+  }
+
+  def askUserToJoinGame(): Unit = {
+    View.askUserToJoinGame()
+  }
+
+  def onMatchStart(): Unit = {
+    View.onMatchStart()
+  }
+
+  def isMyTurn: Boolean = {
+    this._myTurn
+  }
+
+  def userTurnBegins(): Unit = {
+    this._myTurn = true
+    View.userTurnBegins()
+  }
+
+  def endMyTurn(): Unit = {
+    this._myTurn = false
+  }
+
+  def turnEndUpdates(): Unit = {
+  }
+
+}
+
+
---
+++ src/main/scala/client/view/View.scala
@@ -0,0 +1,54 @@
+package client.view
+
+import scalafx.application.{JFXApp, Platform}
+
+object View extends JFXApp {
+
+  val mainMenu: MainMenu = new MainMenu
+  var gameBoard: GameView = _
+  stage = mainMenu
+
+
+  //da chiamare quando il greeting ha confermato il login
+  def onLoginResponse(): Unit = {
+    mainMenu.onLoginResponse()
+  }
+
+  //da chiamare quando si può iniziare la partita
+  def onMatchStart(): Unit = {
+    Platform.runLater(() => {
+      stage.close()
+      gameBoard = new GameView()
+      //updateHand(cards)
+    })
+  }
+
+  //chiede all'utente di joinare la partita
+  def askUserToJoinGame(): Unit = {
+    mainMenu.askUserToJoinGame()
+  }
+
+  //Chiamato quando dalla gameboard si vuole tornare nel mainmenu
+  def backToMainMenu(): Unit = {
+    gameBoard.close()
+    stage.show()
+  }
+
+  //chiamato quando inizia il turno dell'utente
+  def userTurnBegins(): Unit = {
+    //TODO: Fare in modo che l'utente possa interagire con la gui
+  }
+
+  //chiamato a fine turno quando il Gameserver broadcasta gli aggiornamenti, è la fine del turno del player per il GameServer
+  def turnEndUpdates(word: List[(LetterTile, Int, Int)]): Unit = {
+    //TODO: Qui va updatata la board facendo vedere la parola giocata
+  }
+
+  def endMyTurn(): Unit = {
+    //TODO: Qui va chiamata la logica di fine turno
+  }
+
+}
+
+class View {}
+
