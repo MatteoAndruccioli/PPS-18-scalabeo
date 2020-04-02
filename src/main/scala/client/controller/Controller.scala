cb669d99c1fa698ae611be2b39e461600e48287e
Michele Borasca <michele.borasca@gmail.com>
2020-04-02 20:02:54 +0200

    [mvc] Aggiunti nuovi metodi nel controller per la gestione delle fasi di gioco

--- src/main/scala/client/controller/Controller.scala
+++ src/main/scala/client/controller/Controller.scala
@@ -2,7 +2,11 @@ package client.controller
 
 import akka.actor.ActorRef
 import client.controller.Messages.ViewToClientMessages
+import client.controller.MoveOutcome.{AcceptedWord, HandSwitchAccepted, HandSwitchRefused, PassReceived, RefusedWord, TimeoutReceived}
 import client.view.View
+import model.Card
+
+import scala.collection.mutable.ArrayBuffer
 
 object Controller {
 
@@ -62,6 +66,43 @@ object Controller {
 
   def collectLetters(): Unit = {
     GameManager.collectLetters()
+  }
+
+  //metodo attraverso cui il Client comunica al controller l'esito della mossa inviata al GameServer
+  def moveOutcome[A >: MoveOutcome](outcome: A):Unit = outcome match {
+    case _: RefusedWord => _
+    case _: HandSwitchRefused => _
+    case _: AcceptedWord => _
+    case _: HandSwitchAccepted => _
+    case _: PassReceived => _
+    case _: TimeoutReceived => _
+  }
+
+  def onConnectionFailed : Unit = ???
+
+}
+
+// tipo dell'esito di una mossa, contiene informazioni che indicano la risposta del server alla mossa compiuta dall'utente
+sealed trait MoveOutcome
+object MoveOutcome{
+  //casi in cui la mossa effettuata non viene accettata dal'utente
+  case class RefusedWord() extends MoveOutcome //utente aveva indicato la composizione di una parola che viene rifìutata
+  case class HandSwitchRefused() extends MoveOutcome //utente aveva richiesto un cambio delle tessere nella mano, rifiutato dal GameServer
+
+  //casi in cui la mossa viene accettata dall'utente
+  //utente aveva indicato la composizione di una parola che viene accettata, GameServer passa inoltre la nuova mano di tessere disponibili all'utente
+  case class AcceptedWord(hand:ArrayBuffer[Card]) extends MoveOutcome
+  //utente aveva richiesto un cambio delle tessere nella mano, GameServer passa inoltre la nuova mano di tessere disponibili all'utente
+  case class HandSwitchAccepted(hand:ArrayBuffer[Card]) extends MoveOutcome
+  //utente aveva espresso intenzione di passare il turno, GameServer ne ha preso atto
+  case class PassReceived() extends MoveOutcome
+  //timer è scaduto, GameServer ne ha preso atto
+  case class TimeoutReceived() extends MoveOutcome
+
+  sealed trait ServerDown
+  object ServerDown{
+    case class GreetingServerDown() extends ServerDown
+    case class GameServerDown() extends ServerDown
   }
 
 }
