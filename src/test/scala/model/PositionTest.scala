2fa04dd527a6501ec134527104356ece62283d6e
alessia.ventani <aleventa97@gmail.com>
2020-04-17 21:23:44 +0200

    [Test] Refactor test di Position

--- src/test/scala/model/PositionTest.scala
+++ src/test/scala/model/PositionTest.scala
@@ -4,20 +4,17 @@ import org.scalatest._
 import model.Directions.E
 
 class PositionTest extends FlatSpec {
-
   "The Position bonus " should " be equal to bonus of the board" in {
     val x = 1
     val y = 1
     val position = Position(x,y)
     assert(position.bonus.equals(boardConstants.boardBonus.get(x,y).getOrElse("DEFAULT")))
   }
-
   "Coordinates of position " should " be checked" in {
     val positionGood = Position(1,1)
     val positionWrong = Position(20,20)
     assert(positionGood.isValidPosition() && !positionWrong.isValidPosition())
   }
-
   "Position " should " be shifed with position or with direction" in {
     val position = Position(1,1)
     val shiftedCoordinatePosition = position.shift(1,0)
