30238e71d5cc70002afbf89c4923ce8993872870
Matteo Andruccioli <matteo.andruccioli2@studio.unibo.it>
2020-04-18 18:23:41 +0200

    [ControllerRefactor] Fixata inizializzazione di Controller

--- src/main/scala/client/ClientLauncher.scala
+++ src/main/scala/client/ClientLauncher.scala
@@ -2,6 +2,7 @@ package client
 
 import akka.actor.{ActorSystem, Props}
 import client.controller.Controller
+import client.controller.ControllerLogic.CleverMind
 import com.typesafe.config.ConfigFactory
 
 //launcher del client
@@ -14,5 +15,5 @@ object ClientLauncher extends App{
 
   val client = system.actorOf(Props.create(classOf[ClientActor]), "client")
 
-  Controller.init(client)
+  Controller.init(client, CleverMind())
 }
