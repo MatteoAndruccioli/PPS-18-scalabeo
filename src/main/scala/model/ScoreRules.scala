09d13af5bb732cd9e2208306343bd27453a74b29
alessia.ventani <aleventa97@gmail.com>
2020-04-19 12:45:36 +0200

    [Model] Refactor scoreRules

--- src/main/scala/model/ScoreRules.scala
+++ src/main/scala/model/ScoreRules.scala
@@ -1,41 +1,40 @@
 package model
 
-import model.Card
 import scala.collection.mutable.ArrayBuffer
 
-
 package object scoreConstants {
-
+  // bonus posizione
   val letterForTwo: String = "2L"
   val letterForThree: String = "3L"
   val wordForTwo: String = "2P"
   val wordForThree: String = "3P"
+  // bonus lunghezza
   val bonusLenght8 = 50
   val bonusLenght7 = 30
   val bonusLenght6 = 10
-  val scarabeo = "[a-zA-Z]"
+  // bonus prima parola
   val firstWordBonus = 2
+  // bonus scarabeo
   val bonusScarabeoWord = 100
   val bonusWithoutScarabeo = 10
+  // costanti scarabeo
+  val scarabeo = "[a-zA-Z]"
   val scarabeoWord = "scarabeo"
 }
 
 package object scoreRules{
-
   // regola assegnazione valore moltiplicatore del punteggio della parola
   def wordMultiplier(positionBonus: String): Int = positionBonus match{
     case scoreConstants.wordForTwo => 2
     case scoreConstants.wordForThree => 3
     case _ => 0
   }
-
   // regola assegnazione valore moltiplicatore del punteggio della lettera
   def letterMultiplier(positionBonus: String): Int = positionBonus match{
     case scoreConstants.letterForTwo => 2
     case scoreConstants.letterForThree => 3
     case _ => 1
   }
-
   // regola assegnazione bonus lunghezza della parola inserita
   def lenghtBonus(word: ArrayBuffer[(Card, String)]) : Int = word.length match{
     case 8 => scoreConstants.bonusLenght8 + noScarabeoCardBonus(word)
@@ -43,14 +42,11 @@ package object scoreRules{
     case 6 => scoreConstants.bonusLenght6 + noScarabeoCardBonus(word)
     case _ => 0
   }
-
   // regola assegnazione bonus per la composizione della parola senza scarabeo
   def noScarabeoCardBonus(word: ArrayBuffer[(Card, String)]): Int =
     if (word exists (tuple => tuple._1.letter == scoreConstants.scarabeo)) 0 else scoreConstants.bonusWithoutScarabeo
-
   // regola assegnazione bonus per parola formata == SCARABEO
   def wordScarabeoBonus(word: ArrayBuffer[(Card, String)]): Int =  if ((for (tuple <- word; playedWord <- tuple._1.letter) yield playedWord).mkString("").toLowerCase.equals(scoreConstants.scarabeoWord)) scoreConstants.bonusScarabeoWord else 0
-
   // regola assegnazione bonus per la prima parola composta nella partita
   def bonusFirstWord(): Int = scoreConstants.firstWordBonus
 }
