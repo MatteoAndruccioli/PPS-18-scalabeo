add65e4c12bc4740fe0126cb2d7698dfc906909f
alessia.ventani <aleventa97@gmail.com>
2020-04-19 16:53:34 +0200

    [Model] refactor Position

---
+++ src/main/scala/model/PositionImpl.scala
@@ -0,0 +1,58 @@
+package model
+
+
+// Direzione in cui ci può movere nel tabellone
+sealed trait Direction {
+  val shift : (Int, Int)
+}
+
+package object Directions {
+  case object N extends Direction { val shift: (Int, Int) = (0, -1) }  // Nord
+  case object E extends Direction { val shift: (Int, Int) = (1, 0) }   // Est
+  case object S extends Direction { val shift: (Int, Int) = (0, 1) }   // Sud
+  case object W extends Direction { val shift: (Int, Int) = (-1, 0) }  // Ovest
+}
+
+// Posizione di una lettera nella board
+sealed trait Position {
+  def coord: (Int, Int)
+  def bonus: String
+  // metodo per muoversi da una posizione specificando delle coordinate numeriche
+  def shiftByCoordinates(diff: (Int, Int)) : Option[PositionImpl]
+  // metodo per muoversi da una posizione utilizzando una direzione
+  def shiftByDirection(dir: Direction) : Option[PositionImpl]
+  // metodo per controllare se le coordinate di una posizione sono valide
+  def isValidPosition: Boolean
+}
+
+// Posizione sulla board
+case class PositionImpl(row : Int, col : Int) extends Position {
+  val _coord: (Int, Int) = (row - 1, col - 1)
+  val _bonus: String = boardConstants.boardBonus.get(row,col).getOrElse("DEFAULT")
+  override def coord : (Int, Int) = _coord
+  override def bonus : String = _bonus
+  // metodo per muoversi da una posizione specificando delle coordinate numeriche
+  override def shiftByCoordinates(diff: (Int, Int)) : Option[PositionImpl] =
+    Option(PositionImpl(_coord._1 + diff._1, _coord._2 + diff._2))
+  // metodo per muoversi da una posizione utilizzando una direzione
+  override def shiftByDirection(dir: Direction) : Option[PositionImpl] =
+    shiftByCoordinates(dir.shift)
+  // metodo per controllare se le coordinate di una posizione sono valide
+  override def isValidPosition : Boolean = _coord match {
+    case (_,_) if _coord._1 >= 0 && _coord._1 <=16 && _coord._2 >= 0 && _coord._2 <=16 => true
+    case _ => false
+  }
+}
+
+// iterator per le posizioni
+class PositionIterator(pos: PositionImpl, dir: Direction, until: Option[PositionImpl]) extends Iterator[PositionImpl] {
+  var current: PositionImpl = pos
+  def hasNext: Boolean = {
+    val next = current.shiftByDirection(dir)
+    next.nonEmpty && (until.isEmpty || next != until)
+  }
+  def next(): PositionImpl = {
+    current = current.shiftByDirection(dir).get
+    current
+  }
+}
--- src/test/scala/model/PositionTest.scala
+++ src/test/scala/model/PositionTest.scala
@@ -8,23 +8,23 @@ class PositionTest extends FlatSpec {
   "The Position bonus " should " be equal to bonus defined for the position" in {
     val x = 1
     val y = 1
-    val position = Position(x,y)
-    assert(position.bonus == boardConstants.boardBonus.get(x,y).getOrElse("DEFAULT"))
+    val position = PositionImpl(x,y)
+    assert(position._bonus == boardConstants.boardBonus.get(x,y).getOrElse("DEFAULT"))
   }
   "A validity check " should " be performed on position coordinates" in {
-    val positionGood = Position(1,1)
-    val positionWrong = Position(20,20)
-    assert(positionGood.isValidPosition() && !positionWrong.isValidPosition())
+    val positionGood = PositionImpl(1,1)
+    val positionWrong = PositionImpl(20,20)
+    assert(positionGood.isValidPosition && !positionWrong.isValidPosition)
   }
   "Position on the boord" should " be changed according to the direction" in {
-    val position = Position(1,1)
-    val shiftedPositionByDirection = position.shift(E)
-    assert(shiftedPositionByDirection.get == Position(1,0))
+    val position = PositionImpl(1,1)
+    val shiftedPositionByDirection = position.shiftByDirection(E)
+    assert(shiftedPositionByDirection.get == PositionImpl(1,0))
   }
   "Position on the boord" should " be changed according to new coordinates" in {
-    val position = Position(1,1)
-    val shiftedPositionByNumber = position.shift((1,1))
-    assert(shiftedPositionByNumber.get == Position(1,1))
+    val position = PositionImpl(1,1)
+    val shiftedPositionByNumber = position.shiftByCoordinates((1,1))
+    assert(shiftedPositionByNumber.get == PositionImpl(1,1))
   }
 }
 
