91c609fd818b8e859d279fbb256b5cf115a660ed
Michele Borasca <michele.borasca@gmail.com>
2020-04-20 14:43:39 +0200

    [refactor] Refactor della classe BoardAndPlayerPanel

--- src/main/scala/client/view/BoardAndPlayerPanel.scala
+++ src/main/scala/client/view/BoardAndPlayerPanel.scala
@@ -5,63 +5,45 @@ import scalafx.scene.layout.{BorderPane, HBox, VBox}
 import scala.collection.mutable.ArrayBuffer
 
 class BoardAndPlayerPanel(cards: ArrayBuffer[(String, Int)]) extends BorderPane {
-
+  private val LETTER_SIZE = 60
+  private val LETTER_PLACEHOLDER_VALUE = 0
+  private val LETTERS_SPACING = 8
   private val board = new BoardPanel
   stylesheets = List("/style/BPStyle.css")
   styleClass += "body"
 
-  val myHand: HBox = new HBox(8) {
+  val myHand: HBox = new HBox(LETTERS_SPACING) {
     styleClass += "my-hand"
   }
-  cards.zipWithIndex.foreach(c => myHand.getChildren.add(LetterTile(60, c._1._1, c._1._2.toString, c._2, LetterStatus.inHand)))
+
+  cards.zipWithIndex.foreach(c => myHand.getChildren.add(LetterTile(LETTER_SIZE, c._1._1, c._1._2.toString, c._2, LetterStatus.inHand)))
   BoardInteraction.setMyHand(myHand)
 
-  val opponentHandTop: HBox = new HBox(8) {
+  val opponentHandTop: HBox = new HBox(LETTERS_SPACING) {
     styleClass += "top-opponent"
-    children = List(
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder)
-    )
+    children = placeholderhand()
   }
 
-  val opponentHandLeft: VBox = new VBox(8) {
+  val opponentHandLeft: VBox = new VBox(LETTERS_SPACING) {
     styleClass += "left-opponent"
-    children = List(
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder)
-    )
+    children = placeholderhand()
   }
 
-  val opponentHandRight: VBox = new VBox(8) {
+  val opponentHandRight: VBox = new VBox(LETTERS_SPACING) {
     styleClass += "right-opponent"
-    children = List(
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder),
-      LetterTile(60, "", "", 0, LetterStatus.placeHolder)
-    )
+    children = placeholderhand()
   }
+
   center = board
   left = opponentHandLeft
   right = opponentHandRight
   top = opponentHandTop
   bottom = myHand
+
+  private def placeholderhand(): List[LetterTile] = {
+    val list = List.fill(8)(LetterTile(LETTER_SIZE, "", "", LETTER_PLACEHOLDER_VALUE, LetterStatus.placeHolder))
+    list
+  }
 
   def updateBoard(word: List[(LetterTile, Int, Int)]): Unit = {
     board.updateBoard(word)
