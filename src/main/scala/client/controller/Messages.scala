8b4d57532d28c4e9a2306fc147ca4eafc24947c3
Matteo Andruccioli <matteo.andruccioli2@studio.unibo.it>
2020-04-24 13:13:36 +0200

    [refactor] Migliorato stile commenti ViewToClientMessages

--- src/main/scala/client/controller/Messages.scala
+++ src/main/scala/client/controller/Messages.scala
@@ -3,21 +3,23 @@ package client.controller
 import shared.Move
 
 object Messages {
+  /** Trait dei messaggi inviati dalla UI a ClientActor */
   sealed trait ViewToClientMessages
+  /** Contiene l'implementazione di messaggi inviati da UI a ClientActor */
   object ViewToClientMessages {
-    //messaggio contenente lo username scelto dal giocatore
+    /** messaggio contenente lo username scelto dal giocatore */
     case class UsernameChosen(username: String) extends ViewToClientMessages
-    //giocatore chiede di essere messo in coda per giocare
+    /** giocatore chiede di essere messo in coda per giocare */
     case class JoinQueue() extends ViewToClientMessages
-    //giocatore esprime disponibilità a entrare in una partita
+    /** giocatore esprime disponibilità a entrare in una partita */
     case class UserReadyToJoin(ready:Boolean) extends ViewToClientMessages
-    //giocatore vuole giocare una nuova partita
+    /** giocatore vuole giocare una nuova partita */
     case class PlayAgain(userWantsToPlay:Boolean) extends ViewToClientMessages
-    //il giocatore ha chiuso il gioco forzatamente
+    /** il giocatore ha chiuso il gioco forzatamente */
     case class UserExited() extends  ViewToClientMessages
-    //giocatore indica la propria mossa
+    /** giocatore indica la propria mossa */
     case class UserMadeHisMove(move: Move) extends ViewToClientMessages
-    //messaggio che il giocatore vuole inviare in chat
+    /** messaggio che il giocatore vuole inviare in chat */
     case class ChatMessage(message: String) extends ViewToClientMessages
   }
 }
